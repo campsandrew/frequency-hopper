118f77b41a80f047794c4c9feb9e6ace5692281a
Andrew Camps <andrewcamps@dhcp-10-142-145-42.uawifi.arizona.edu>
2019-04-23 15:38:26 -0700

    testing

--- src/pluto_one.m
+++ src/pluto_one.m
@@ -1,79 +1,67 @@
-%% Variable Initializations
-visuals = true;                                         % Debug flag
-loFreq = 2.4e9;                                         % LO Center Frequency
-sampleRate = 1e6;
-samplesPerSymbol = 2;
-filterSymbolSpan = 4;
-modOrder = 1;
-frameSize = 2^10;
+%% General simulation parameters
+Rsym = 0.2e6;              % Symbol rate in Hertz
+ModulationOrder = 4;       % QPSK alphabet size
+Interpolation = 2;         % Interpolation factor
+Decimation = 1;            % Decimation factor
+Tsym = 1 / Rsym;           % Symbol time in sec
+Fs = Rsym * Interpolation; % Sample rate
 
-% Object Variables
-dbpskMod = comm.DBPSKModulator();                       % Modulator
-dbpskDemod = comm.DBPSKDemodulator();                   % Demodulator
-rx = sdrrx('Pluto',...                                  % Pluto Receiver
-    'CenterFrequency', loFreq, ...
-    'SamplesPerFrame', frameSize, ...    
-    'OutputDataType', 'double', ...
-    'BasebandSampleRate', sampleRate);
-tx = sdrtx('Pluto', ...                                 % Pluto Transmitter
-    'CenterFrequency', loFreq, ...
-    'BasebandSampleRate', sampleRate);
-txFlt = comm.RaisedCosineTransmitFilter(...             % TX Filter
-    'OutputSamplesPerSymbol', samplesPerSymbol,...
-    'FilterSpanInSymbols', filterSymbolSpan);
-symbolSync = comm.SymbolSynchronizer(...
-    'SamplesPerSymbol', samplesPerSymbol, ...
-    'NormalizedLoopBandwidth', 0.01, ...
-    'DampingFactor', 1.0, ...
-    'TimingErrorDetector','Zero-Crossing (decision-directed)');
-coarseSync = comm.CoarseFrequencyCompensator('Modulation','BPSK', ...
-    'FrequencyResolution', 1, ...
-    'SampleRate', sampleRate * samplesPerSymbol);
-fineSync = comm.CarrierSynchronizer('DampingFactor', 0.7, ...
-    'NormalizedLoopBandwidth', 0.005, ...
-    'SamplesPerSymbol', samplesPerSymbol, ...
-    'Modulation','BPSK');
+%% Frame Specifications
+BarkerCode = [+1 +1 +1 +1 +1 -1 -1 +1 +1 -1 +1 -1 +1];
+BarkerLength = length(BarkerCode);
+HeaderLength = BarkerLength * 2;
 
-% Visual Objects
-% sa = dsp.SpectrumAnalyzer('SampleRate', sampleRate, 'ShowLegend', true);
-cdRef = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
-    'Name','Reference');
-cdPre = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
-    'Name','Baseband with Timing Offset');
-cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
-    'Name','Corrected Timing Offset');
-cdRef.Position(1) = 50;
-cdPre.Position(1) = cdRef.Position(1) + cdRef.Position(3) + 10;
-cdPost.Position(1) = cdPre.Position(1) + cdPre.Position(3) + 10;
 
-%% Transmit Repeat Useful Data
-% tx.transmitRepeat();
+Message = 'Hello World';
+MessageLength = length(Message) + 5;
+NumberOfMessage = 100;
+PayloadLength = NumberOfMessage * MessageLength * 7;
+FrameSize = (HeaderLength + PayloadLength) / log2(ModulationOrder);
 
-%% Receive Data
-while true
-    rxFiltData = rxFlt(rx());                      % Receive data
-    
-    % Apply corrections
-    rxData = symbolSync(rxFiltData);               % Timing Correction
-    tSize = size(rxData);                          % Correct frame sizing after timing
-    if tSize(1) < frameSize
-        stuff = frameSize - tSize(1);
-        rxData = [rxData; zeros(stuff, 1)];
-    end
-    if tSize(1) > frameSize
-       remove = tSize(1) - frameSize;
-       rxData = rxData(1:end - remove);
-    end
-    rxData = coarseSync(rxData);                   % Coarse Frequency Correction
-    rxData = fineSync(rxData);                     % Fine Frequency Correction 
-    %TODO: Frame syncronization
-    rxDemodData = dbpskDemod(rxFiltData);              % Demodulate Data
-    
-    % Frame by frame graphs
-    if visuals
-%         cdRef(txFiltData);              % After TX Filter
-%         cdPre(txData);                  % Sent Data
-        cdPost(rxData);                 % Data After Corrections
-%         pause(0.1);
-    end 
+%% Tx parameters
+RolloffFactor = 0.5;
+ScramblerBase = 2;
+ScramblerPolynomial = [1 1 1 0 1];
+ScramblerInitialConditions = [0 0 0 0];
+RaisedCosineFilterSpan = 10;
+
+%% Message generation
+msgSet = zeros(100 * MessageLength, 1); 
+for msgCnt = 0 : 99
+    msgSet(msgCnt * MessageLength + (1 : MessageLength)) = ...
+        sprintf('%s %03d\n', Message, msgCnt);
 end
+integerToBit = comm.IntegerToBit(7, 'OutputDataType', 'double');
+MessageBits = integerToBit(msgSet);
+
+% Pluto TX
+tx = sdrtx('Pluto', ...
+           'CenterFrequency', 915e6, ...
+           'BasebandSampleRate', Fs, ...
+           'SamplesPerFrame', Interpolation * FrameSize, ...
+           'Gain', 0);
+hTx = QPSKTransmitter(...
+    'UpsamplingFactor',             Interpolation, ...
+    'RolloffFactor',                RolloffFactor, ...
+    'RaisedCosineFilterSpan',       RaisedCosineFilterSpan, ...
+    'MessageBits',                  MessageBits, ...
+    'MessageLength',                MessageLength, ...
+    'NumberOfMessage',              NumberOfMessage, ...
+    'ScramblerBase',                ScramblerBase, ...
+    'ScramblerPolynomial',          ScramblerPolynomial, ...
+    'ScramblerInitialConditions',   ScramblerInitialConditions);
+
+
+FrameTime = Interpolation * FrameSize / Fs;
+StopTime  = 1000;
+currentTime = 0;
+while currentTime < StopTime
+    data = step(hTx);
+    step(tx, data);
+    currentTime = currentTime + FrameTime;
+end
+    
+release(hTx);
+release(radio);
+
+
